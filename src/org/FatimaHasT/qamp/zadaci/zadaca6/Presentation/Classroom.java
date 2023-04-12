2891bf9f3eb3be381ded1e7fecb95d5ac739e855
Fatima Hasanovic <fatimahasanovic1994@gmail.com>
2023-04-13 01:44:43 +0200

    last update

--- src/org/FatimaHasT/qamp/zadaci/zadaca6/Presentation/Classroom.java
+++ src/org/FatimaHasT/qamp/zadaci/zadaca6/Presentation/Classroom.java
@@ -1,2 +1,32 @@
-package org.FatimaHasT.qamp.zadaci.zadaca6.Presentation;public class Classroom {
+package org.FatimaHasT.qamp.zadaci.zadaca6.Presentation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Classroom {
+    private List<Student> students;
+
+    public Classroom() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public boolean isPresent(String name) {
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printClassroom() {
+        System.out.println("Classroom:");
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
 }
--- src/org/FatimaHasT/qamp/zadaci/zadaca6/task1/MainBook.java
+++ src/org/FatimaHasT/qamp/zadaci/zadaca6/task1/MainBook.java
@@ -1,2 +1,27 @@
-package org.FatimaHasT.qamp.zadaci.zadaca6.task1;public class MainBook {
+package org.FatimaHasT.qamp.zadaci.zadaca6.task1;
+
+public class MainBook {
+
+    public static void main(String[] args) {
+
+        final Book book1 = new Book("Na Drini ćuprija", 9789958300092L, "Ivo Andrić");
+        final Book book2 = new Book("Znakovi pored puta", 9789958693928L, "Ivo Andrić");
+        final Book book3 = new Book("Sjećanja, sećanja", 9788617111863L, "Aleksa Šantić");
+        final Book book4 = new Book("Legenda o Ali-paši", 9788610001317L, "Enver Čolaković");
+
+        Bookshelf bookshelf = new Bookshelf();
+        bookshelf.addBook(book1);
+        bookshelf.addBook(book2);
+        bookshelf.addBook(book3);
+        bookshelf.addBook(book4);
+
+        System.out.println(bookshelf.containsBook("Na Drini ćuprija"));
+        System.out.println(bookshelf.containsBook("Gospođica"));
+        System.out.println(bookshelf.containsBook(9789958693928L));
+        System.out.println(bookshelf.containsBook(9788679340036L));
+
+        System.out.println("\nPrinting the bookshelf:");
+        bookshelf.printBookshelf();
+
+    }
 }
